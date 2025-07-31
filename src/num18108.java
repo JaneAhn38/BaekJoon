d6be62543807bc19540d10c452e64543c136a06d
JaneAhn38 <jjung9874@gmail.com>
2025-07-31 23:29:00 +0900

    num18108

---
+++ src/num18108.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class num18108 {
+    public static void main(String[] args){
+        Scanner a = new Scanner(System.in);
+        int y = a.nextInt() + 543 ;
+        System.out.println(y);
+
+    }
+}
