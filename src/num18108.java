4d60a6fc8c0447228f47351928d528a488766cc1
JaneAhn38 <jjung9874@gmail.com>
2025-08-01 13:01:59 +0900

    num18108

--- src/num18108.java
+++ src/num18108.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class num18108 {
     public static void main(String[] args){
         Scanner a = new Scanner(System.in);
-        int y = a.nextInt() + 543 ;
+        int y = a.nextInt() - 543 ;
         System.out.println(y);
 
     }
