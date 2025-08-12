8c4f1696b04239c2f25bb7974643f2c4e084c50e
JaneAhn38 <jjung9874@gmail.com>
2025-08-12 18:56:46 +0900

    꼬마 정민

---
+++ src/num11382.java
@@ -0,0 +1,21 @@
+import java.io.*;
+public class num11382 {
+    public static void main (String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader (System.in));
+
+        String[] input = br.readLine().split(" ");
+
+        long a = Long.parseLong(input[0]);
+        long b = Long.parseLong(input[1]);
+        long c = Long.parseLong(input[2]);
+
+        System.out.println(a + b + c);
+
+
+
+
+
+    }
+
+}
