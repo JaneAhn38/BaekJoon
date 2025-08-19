c353463e8ea60d2dde6071d478db843d8bbc5106
JaneAhn38 <jjung9874@gmail.com>
2025-08-19 23:36:46 +0900

    두 수 비교하기

---
+++ src/num1330.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class num1330 {
+    public static void main(String args[]){
+        Scanner a = new Scanner(System.in);
+        int A = a.nextInt();
+        int B = a.nextInt();
+
+        if (A > B) System.out.println(">");
+        if (A < B) System.out.println("<");
+        if (A == B) System.out.println("==");
+
+    }
+}
